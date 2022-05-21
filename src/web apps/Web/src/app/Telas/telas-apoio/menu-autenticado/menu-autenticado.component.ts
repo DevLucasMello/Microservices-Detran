0a2a4e5d44b02c1f3b360d2db850290ec4408c20
Lucas de Mello Santos <lucas.sports@hotmail.com>
2022-05-21 18:59:35 -0300

    Telas Login-Registro finalizadas

---
+++ src/web apps/Web/src/app/Telas/telas-apoio/menu-autenticado/menu-autenticado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-autenticado',
+  templateUrl: './menu-autenticado.component.html',
+  styleUrls: ['./menu-autenticado.component.css']
+})
+export class MenuAutenticadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/web apps/Web/src/app/app-routing.module.ts
+++ src/web apps/Web/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcessoNegadoComponent } from './Telas/telas-apoio/acesso-negado/acesso-negado.component';
+import { LoginComponent } from './Telas/telas-principais/login/login/login.component';
+import { RegistroComponent } from './Telas/telas-principais/login/registro/registro.component';
+import { NotFoundComponent } from './Telas/telas-apoio/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', component: LoginComponent},
+
+  { path: 'login', component: LoginComponent},
+
+  { path: 'registro', component: RegistroComponent},
+
+  { path: 'home', loadChildren: () => import('./Telas/telas-principais/home/home-module').then(m => m.HomeModule) },
+
+  { path: 'acesso-negado', component: AcessoNegadoComponent },
+  
+  { path: 'nao-encontrado', component: NotFoundComponent },
+
+  { path: '**', component: NotFoundComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
